62039ef869b8719dce5cf1132c7279f4bf548e01
Leo <leo.chen@twsc.io>
2023-07-13 03:13:29 +0000

    add get_token.py

---
+++ get_token.py
@@ -0,0 +1,38 @@
+import json
+import re
+from httpx import main
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained('bigscience/tokenizer')
+
+
+def get_token(inputs, targets):
+    tokens = tokenizer.tokenize(inputs+targets)
+    print(len(tokens), " tokens.")
+
+
+def main():
+    error_list = []
+    # load file
+    with open('get_token_inputs.json', 'r') as file:
+        for index, line in enumerate(file):
+            try:
+                data = json.loads(line)
+                inputs = data['inputs']
+                targets = data['targets']
+                get_token(inputs, targets)
+            except:
+                # print(f'line:{index+1}, not match json format')
+                error_list.append(str(index+1))
+                matches = re.findall(
+                    r'\s*"inputs"\s*:\s*"(.+)",\s*"targets"\s*:\s*"(.+)\s*"', line)
+                for match in matches:
+                    inputs = match[0]
+                    targets = match[1]
+                    get_token(inputs, targets)
+    if error_list != []:
+        print(
+            f"Please check data!!! lines: [{','.join(error_list)}] format are not correct")
+
+
+if __name__ == '__main__':
+    main()
